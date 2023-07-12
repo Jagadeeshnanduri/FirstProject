88cab882a4e4d387ccf2a49fb01ebbbf494e2cd3
Jagadeeshnanduri <saijagadeeshnanduri@gmail.com>
2023-07-12 19:37:33 +0530

    alert1

---
+++ AlertDemo1.java
@@ -0,0 +1,39 @@
+package programs;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class AlertDemo1
+
+{
+
+	@Test
+	public void LabelText3text() throws InterruptedException
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.meghanabus.in/index.html");
+		WebElement manageBookings=driver.findElement(By.xpath("//a[@title='Manage Bookings']"));
+		manageBookings.click();
+		Thread.sleep(4000);
+
+		WebElement searchButton=driver.findElement(By.xpath("//button[@type='submit']"));
+		searchButton.click();
+
+		Thread.sleep(4000);
+		Alert alert=driver.switchTo().alert();
+		String Alert=alert.getText();
+		System.out.println("Alert text is : "+alert);
+		alert.dismiss();
+
+		WebElement text=driver.findElement(By.xpath("//span[text()='Please enter the details for Print, Update, Cancel and Pre/Postpone Ticket.']"));
+		text.getText();
+
+
+
+	}
+
+}
